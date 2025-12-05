d01a5033771eafecfcc0cc231ab525d6ed3307f6
Matt Paffett <matt1.paffett@hotmail.co.uk>
2025-12-05 20:13:57 +0000

    updated hero section

--- app/page.js
+++ app/page.js
@@ -3,7 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <button className="btn">Get started!!!! 🤞🏼</button>
+      <h1>Collect customer feedback to build better products</h1>
+      <div>
+        Create a feedback board in minutes, proiritise features, and build
+        products your customers will love.
+      </div>
     </main>
   );
 }
