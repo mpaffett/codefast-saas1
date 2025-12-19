03a6d908b91385c0756498cf6a5c0b5bc6365935
Matt Paffett <matt1.paffett@hotmail.co.uk>
2025-12-19 10:50:32 +0000

    polish homepage

--- components/ButtonLogin.js
+++ components/ButtonLogin.js
@@ -1,15 +1,18 @@
 import Link from "next/link";
 
-const ButtonLogin = ({ name, isLoggedIn }) => {
+const ButtonLogin = ({ name, isLoggedIn, extraStyle }) => {
   if (isLoggedIn) {
     return (
-      <Link href="/dashboard" className="btn btn-primary">
+      <Link
+        href="/dashboard"
+        className={`btn btn-primary ${extraStyle ? extraStyle : ""} `}
+      >
         Welcome back, {name}!
       </Link>
     );
   }
 
-  return <button className="btn btn-primary">Login</button>;
+  return <button className="btn btn-primary">Log in</button>;
 };
 
 export default ButtonLogin;
